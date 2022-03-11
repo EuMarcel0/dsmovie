c56ebbf725fc4c1b33ba2d6a2402e7967771ba7f
Marcelo Silva <marcelo.dev.js@hotmail.com>
2022-03-11 20:16:51 -0300

    First request

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,7 +1,17 @@
 import Pagination from 'components/pagination';
 import MovieCard from 'components/moviecards';
+import axios from 'axios';
+import { BASE_URL } from 'utils/requests';
 
 function Listing() {
+    
+    // FORMA ERRADA
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response =>{
+            console.log(response.data);
+        
+        });
+
 
     return (
         <>
