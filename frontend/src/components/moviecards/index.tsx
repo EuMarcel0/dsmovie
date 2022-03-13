b446f1bdee4f66fbe9517179acc2837e3066eddc
Marcelo Silva <marcelo.dev.js@hotmail.com>
2022-03-13 13:32:52 -0300

    Props

--- frontend/src/components/moviecards/index.tsx
+++ frontend/src/components/moviecards/index.tsx
@@ -1,14 +1,14 @@
 import MovieScore from 'components/moviescore';
 import { Link } from 'react-router-dom';
-const movie = {
-    id: 1,
-    image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcher",
-    count: 2,
-    score: 4.5
-};
+import { Movie } from 'types/movie';
 
-function movieCards(){
+type Props = {
+    movie: Movie;
+}
+
+
+function movieCards( { movie }: Props) {
+    
     return(
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
@@ -22,6 +22,6 @@ function movieCards(){
             </div>
         </div>
     );
-}
+};
 
 export default movieCards;
